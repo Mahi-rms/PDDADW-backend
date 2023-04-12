3730239a21e08b0be4c837f9ce5aa3e9c192bf74
Mahi-rms <mahesh2019@iiitkottayam.ac.in>
2023-04-12 23:43:19 +0530

    Updated Requirements

--- backend/settings.py
+++ backend/settings.py
@@ -64,8 +64,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = ( "http://127.0.0.1:8000",)
 
-CSRF_TRUSTED_ORIGINS = [
-]
+CSRF_TRUSTED_ORIGINS = ["https://pddadw-backend-production.up.railway.app"]
 
 CORS_ALLOW_METHODS = (
     'DELETE',
